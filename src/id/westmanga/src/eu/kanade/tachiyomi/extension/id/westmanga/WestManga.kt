5e22b947658c8b9c00a2ca52bb5333480cfef861
tumbal-page <tumbal2827@gmail.com>
2026-06-14 14:42:31 +0700

    idk

--- src/id/westmanga/src/eu/kanade/tachiyomi/extension/id/westmanga/WestManga.kt
+++ src/id/westmanga/src/eu/kanade/tachiyomi/extension/id/westmanga/WestManga.kt
@@ -19,7 +19,7 @@ import javax.crypto.spec.SecretKeySpec
 class WestManga : HttpSource() {
     override val name = "West Manga"
     override val baseUrl = "https://westmanga.cc"
-    private val apiUrl = "https://data.westmanga.cc"
+    private val apiUrl = "https://storage.westmanga.blog"
     override val lang = "id"
     override val id = 8883916630998758688
     override val supportsLatest = true
